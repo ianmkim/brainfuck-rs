64d94a6db3593b82ad8d5fa5b7c5fc7cc6449c8f
parv <42292798+parvusvox@users.noreply.github.com>
2021-08-23 13:27:12 -0400

    added benchmark data and method

---
+++ README.md
@@ -0,0 +1,10 @@
+# Brainfuck-rs
+A tiny brainfuck interpreter written in Rust
+
+# Benchmark
+```
+cargo run examples/helloworld.bf --benchmark 10000
+```
+Executing examples/helloworld.bf 10,000 times:
+ - Total execution time: 6057.83 ms
+ - Average execution time: 0.65 ms
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use std::io::Read;
 use std::collections::HashMap;
 use rayon::prelude::*;
+use indicatif::{ProgressBar, ProgressStyle};
 
 use std::fs;
+use std::time::{Duration, Instant};
 
 use clap::{Arg, App};
 
 const OPCODES:&str= ".,[]<>+-";
 
-fn evaluate(code:String) {
+fn evaluate(code:String, out:bool) {
     let code = clean(code);
     let bmap = build_brace_map(code.clone());
 
@@ -39,7 +41,7 @@ fn evaluate(code:String) {
                     codeptr = bmap[&codeptr];
                 }
             },
-            '.' => print!("{}", cells[cellptr] as char),
+            '.' => if out {print!("{}", cells[cellptr] as char)},
             ',' => {
                 let input:Option<u8> = std::io::stdin()
                     .bytes()
@@ -76,10 +78,10 @@ fn build_brace_map(code: Vec<char>) -> HashMap<usize,usize> {
     } bmap
 }
 
-fn execute(filename:&str){
+fn execute(filename:&str, out:bool){
     let contents = fs::read_to_string(filename)
         .expect("cannot read file");
-    evaluate(contents);
+    evaluate(contents, out);
 }
 
 
@@ -93,10 +95,38 @@ fn main() {
              .about("Brainfuck source file")
              .required(true)
              .index(1))
+        .arg(Arg::new("benchmark")
+             .long("benchmark")
+             .short('b')
+             .value_name("NUM_REPEAT")
+             .about("Benchmark the Brainfuck interpreter using input file"))
         .get_matches();
 
-    if let Some(filename) = matches.value_of("file") {
-        execute(filename);
+    if let Some(benchmark) = matches.value_of("benchmark"){
+        let benchmark_num = benchmark.parse::<u32>().unwrap();
+        let bar = ProgressBar::new(benchmark_num as u64);
+        bar.set_style(ProgressStyle::default_bar()
+                      .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7}")
+                      .progress_chars("##-"));
+
+        let mut time = 0f64; 
+        let filename = matches.value_of("file").unwrap();
+        for _ in 0..benchmark_num{
+            bar.inc(1);
+            let start = Instant::now();
+            execute(filename.clone(), false);
+            let duration = start.elapsed().subsec_nanos() as f64 / 1000000 as f64;
+            time += duration;
+        }
+        bar.finish();
+        println!("Executed {} {} times", filename, benchmark_num);
+        println!("\tTotal execution time: {}ms", time);
+        println!("\tAverage execution time: {}ms", time/benchmark_num as f64);
+
+    } else {
+        if let Some(filename) = matches.value_of("file") {
+            execute(filename, true);
+        }
     }
 }
 
