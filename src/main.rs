10735452f630401a165a8f56f18d991d8263a9aa
parv <42292798+parvusvox@users.noreply.github.com>
2021-08-23 12:17:14 -0400

    finished interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,82 @@
-fn evaluate(String code) {
+use std::io::Read;
+use std::collections::HashMap;
+use rayon::prelude::*;
+
+const OPCODES:&str= ".,[]<>+-";
+
+fn evaluate(code:String) {
+    let code = clean(code);
+    let bmap = build_brace_map(code.clone());
+
+    let mut cells:Vec<u8> = Vec::new();
+    cells.push(0);
+    let mut codeptr:usize = 0;
+    let mut cellptr:usize = 0;
+
+    while codeptr < code.len(){
+        let command = code[codeptr];
+        match command {
+            '>' => {
+                cellptr += 1;
+                if cellptr == cells.len(){
+                    cells.push(0);
+                }
+            },
+            '<' => cellptr = if cellptr <= 0 {0} else {cellptr -1},
+            '+' => cells[cellptr] = if cells[cellptr] < 255 {cells[cellptr] + 1} else {0},
+            '-' => cells[cellptr] = if cells[cellptr] > 0 {cells[cellptr] - 1} else {255},
+            '[' => {
+                if cells[cellptr] == 0 {
+                    codeptr = bmap[&codeptr];
+                }
+            },
+            ']' => {
+                if cells[cellptr] != 0 {
+                    codeptr = bmap[&codeptr];
+                }
+            },
+            '.' => print!("{}", cells[cellptr] as char),
+            ',' => {
+                let input:Option<u8> = std::io::stdin()
+                    .bytes()
+                    .next()
+                    .and_then(| result | result.ok())
+                    .map(|byte| byte as u8);
+                cells[cellptr] = input.unwrap();
+            },
+            _ => {},
+        } codeptr += 1;
+    }
+
+}
+
+fn clean(code:String) -> Vec<char>{
+    code.par_chars()
+        .filter(|&c| OPCODES.contains(c))
+        .collect()
+}
+
+fn build_brace_map(code: Vec<char>) -> HashMap<usize,usize> {
+    let mut brace_stack: Vec<usize> = Vec::new();
+    let mut bmap: HashMap<usize, usize> = HashMap::new();
+    
+    for (i, c) in code.iter().enumerate(){
+        match c {
+            '[' => brace_stack.push(i),
+            ']' => {
+                let start = brace_stack.pop().unwrap();
+                bmap.insert(start, i);
+                bmap.insert(i, start);
+            }, _ => {}
+        }
+    } bmap
+}
+
+fn execute(filename:String) {
     
 }
 
 
 fn main() {
-    println!("Hello, world!");
 }
 
